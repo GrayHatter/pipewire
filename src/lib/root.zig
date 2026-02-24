e8fb106e70976d551a2f4af3a1b87b9cc9119b85
Gregory Mullen <_@gr.ht>
2026-02-24 00:26:39 -0800

    expand zig sided api

--- src/lib/root.zig
+++ src/lib/root.zig
@@ -8,6 +8,111 @@ pub const wrap = @import("wrap");
 
 pub const Logger = @import("Logger.zig");
 
+pub fn init(argv: [:null]const ?[*:0]const u8) void {
+    c.pw_init(@ptrCast(@constCast(&argv.len)), @ptrCast(@constCast(&argv.ptr)));
+}
+
+pub fn deinit() void {
+    c.pw_deinit();
+}
+
+pub const MainLoop = struct {
+    ptr: *c.pw_main_loop,
+
+    pub fn run(ml: MainLoop) !void {
+        if (c.pw_main_loop_run(ml.ptr) != 0) return error.MainLoopRunFailed;
+    }
+
+    pub fn init(props: ?*const c.struct_spa_dict) !MainLoop {
+        if (c.pw_main_loop_new(props)) |loop| {
+            return .{ .ptr = loop };
+        } else return error.UnableToCreateLoop;
+    }
+
+    pub fn raze(ml: MainLoop) void {
+        c.pw_main_loop_destroy(ml.ptr);
+    }
+
+    pub fn getLoop(ml: MainLoop) !Loop {
+        return .{
+            .ptr = c.pw_main_loop_get_loop(ml.ptr) orelse return error.UnableToGetLoop,
+        };
+    }
+};
+
+pub const Loop = struct {
+    ptr: *c.pw_loop,
+};
+
+pub const Context = struct {
+    ptr: *c.pw_context,
+
+    pub fn init(loop: Loop) !Context {
+        return .{
+            .ptr = c.pw_context_new(loop.ptr, null, 0) orelse return error.UnableToCreateContext,
+        };
+    }
+
+    pub fn connect(ctx: Context) !Core {
+        return .{
+            .ptr = c.pw_context_connect(ctx.ptr, null, 0) orelse return error.UnableToConnectCore,
+        };
+    }
+
+    pub fn raze(ctx: Context) void {
+        c.pw_context_destroy(ctx.ptr);
+    }
+};
+
+pub const Core = struct {
+    ptr: *c.pw_core,
+
+    pub fn getRegistry(core: Core) !Registry {
+        return .{
+            .ptr = c.pw_core_get_registry(core.ptr, c.PW_VERSION_REGISTRY, 0) orelse return error.UnableToGetRegistry,
+        };
+    }
+
+    pub fn raze(core: Core) void {
+        _ = c.pw_core_disconnect(core.ptr);
+    }
+};
+
+pub const Registry = struct {
+    ptr: *c.pw_registry,
+
+    pub fn ListenerFn(T: type) type {
+        return *const fn (?*T, u32, u32, ?[:0]const u8, u32, ?*const c.spa_dict) void;
+    }
+
+    pub fn addListener(reg: Registry, T: type, comptime func: ListenerFn(T), reg_listener: *c.spa_hook, usrptr: ?*T) !void {
+        const CFunc = struct {
+            fn wrapper(
+                ptr: ?*anyopaque,
+                id: u32,
+                permissions: u32,
+                name: ?[*:0]const u8,
+                version: u32,
+                props: ?*const c.spa_dict,
+            ) callconv(.c) void {
+                @call(.auto, func, .{
+                    @as(?*T, @ptrCast(@alignCast(ptr))), id, permissions, std.mem.span(name orelse ""), version, props,
+                });
+            }
+        };
+
+        if (c.pw_registry_add_listener(reg.ptr, reg_listener, &.{
+            .version = c.PW_VERSION_REGISTRY_EVENTS,
+            .global = &CFunc.wrapper,
+        }, usrptr) != 0) return error.UnableToAddRegisteryListener;
+    }
+
+    pub fn raze(reg: Registry) void {
+        // This is what the Pipewire examples do
+        _ = c.pw_proxy_destroy(@ptrCast(@alignCast(reg.ptr)));
+    }
+};
+
 pub const Direction = enum(c_uint) { input = c.PW_DIRECTION_INPUT, output = c.PW_DIRECTION_OUTPUT };
 
 pub const Stream = struct {
@@ -54,4 +159,8 @@ comptime {
         _ = &@field(@This(), decl.name);
     }
     _ = &Logger;
+}
+
+test {
+    _ = &std.testing.refAllDecls(@This());
 }
